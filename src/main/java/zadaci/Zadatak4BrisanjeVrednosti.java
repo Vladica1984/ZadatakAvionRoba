1401a6527b212441492504cdaaa6d578c9259a0d
vladica1984 <vladabeba84@gmail.com>
2017-01-19 22:44:15 +0100

    Uradjen zadatak brisanja vrednosti closes#9

--- src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -1,18 +1,44 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Avion;
+import model.Roba;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Vladimir on 1/19/2017.
  */
 public class Zadatak4BrisanjeVrednosti {
+
+    static Dao<Avion,Integer> avionDao;
+    static Dao<Roba,Integer> robaDao;
+
     public static void main(String[] args) {
         ConnectionSource connectionSource = null;
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+
+            avionDao = DaoManager.createDao(connectionSource, Avion.class);
+            robaDao = DaoManager.createDao(connectionSource, Roba.class);
+
+            List<Roba> roba = robaDao.queryForAll();
+            for(Roba r: roba)
+                System.out.println("Roba = " + r);
+
+            List<Roba> pronadjenaRoba = robaDao.queryForEq(Roba.POLJE_NAZIV, "Voda");
+            Roba robaZaBrisanje = pronadjenaRoba.get(0);
+            robaDao.delete(robaZaBrisanje);
+
+            roba = robaDao.queryForAll();
+            for(Roba r: roba)
+                System.out.println("Roba = " +r);
+
+            
 
 
         } catch (Exception e) {
